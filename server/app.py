393585f33830b8cfb2fa2de0a1c55855f9d5927e
samuelgitau <amsamgitau@gmail.com>
2024-10-12 16:27:58 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -28,7 +28,14 @@ def index_articles():
 @app.route('/articles/<int:id>')
 def show_article(id):
 
-    pass
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] += 1
+    
+    if session['page_views'] <= 3:
+        return Article.query.filter(Article.id == id).first().to_dict(), 200
+        
+    return {'message': 'Maximum pageview limit reached'}, 401
+        
 
 if __name__ == '__main__':
     app.run(port=5555)
